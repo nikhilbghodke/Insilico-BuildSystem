ec87f86a6827153ec53e265e507bf037657d8b6f
Nikhil Ghodke <nikhil.ghodke17@vit.edu>
2019-06-02 18:34:29 +0530

    Stopped Git from tracking build directory and .idea/workspace.xml

--- build/docs/javadoc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"Insilico.Build.System","l":"BundleExtension"},{"p":"Insilico.Build.System","l":"CopyFiles"},{"p":"Insilico.Build.System","l":"CustomExtension"},{"p":"Insilico.Build.System","l":"LatestArtifactVersion"},{"p":"Insilico.Build.System","l":"Library"},{"p":"Insilico.Build.System","l":"OSGi"}]
