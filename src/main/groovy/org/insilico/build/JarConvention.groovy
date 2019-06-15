9d8dab98a4bba75115b2dde83d1e805f5ca8d092
Nikhil Ghodke <nikhil.ghodke17@vit.edu>
2019-06-15 18:30:21 +0530

    Made a new Convention and applied it to  custom Task OsgiBundle and jar task

---
+++ src/main/groovy/org/insilico/build/JarConvention.groovy
@@ -0,0 +1,111 @@
+package org.insilico.build
+
+import org.gradle.api.Task
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.Optional
+import org.osgi.framework.Constants
+
+class JarConvention {
+    public String bundleName;
+    public String symbolicName
+    public String vendor
+    public String developer
+    public String license
+    public String version
+    public String contactAddress
+    public String activator
+
+    private  Task task;
+
+    public JarConvention(Task task){
+        this.task=task;
+    }
+
+    @Optional
+    @Input
+    String getActivator() {
+        return activator
+    }
+
+    void setActivator(String activator) {
+        this.activator = activator
+        task.convention.plugins.bundle.bnd(Constants.BUNDLE_ACTIVATOR+"="+this.activator)
+    }
+
+    @Optional
+    @Input
+    String getContactAddress() {
+        return contactAddress
+    }
+
+    void setContactAddress(String contactAddress) {
+        this.contactAddress = contactAddress
+        task.convention.plugins.bundle.bnd(Constants.BUNDLE_CONTACTADDRESS+"="+this.contactAddress)
+    }
+
+    @Input
+    @Optional
+    String getVersion() {
+        return version
+    }
+
+    void setVersion(String version) {
+        this.version = version
+        task.convention.plugins.bundle.bnd(Constants.BUNDLE_VERSION+"="+this.version)
+    }
+
+    @Optional
+    @Input
+    String getLicense() {
+        return license
+    }
+
+    void setLicense(String license) {
+        this.license = license
+        task.convention.plugins.bundle.bnd(Constants.BUNDLE_LICENSE+"="+ this.license)
+    }
+
+    @Optional
+    @Input
+    String getDeveloper() {
+        return developer
+    }
+
+    void setDeveloper(String developer) {
+        this.developer = developer
+        task.convention.plugins.bundle.bnd("Bundle-Developers="+this.developer)
+    }
+
+    @Optional
+    @Input
+    String getVendor() {
+        return vendor
+    }
+
+    void setVendor(String vendor) {
+        this.vendor = vendor
+        task.convention.plugins.bundle.bnd(Constants.BUNDLE_VENDOR+'='+this.vendor)
+    }
+
+    @Optional
+    @Input
+    String getSymbolicName() {
+        return symbolicName
+    }
+
+    void setSymbolicName(String name) {
+        this.symbolicName = name
+        task.convention.plugins.bundle.bnd("Bundle-Name":name)
+    }
+
+    @Optional
+    @Input
+    String getBundleName() {
+        return bundleName
+    }
+
+    void setBundleName(String name) {
+        this.bundleName = name
+    }
+
+}
--- src/main/groovy/org/insilico/build/OsgiBundle.groovy
+++ src/main/groovy/org/insilico/build/OsgiBundle.groovy
@@ -1,113 +1,11 @@
 package org.insilico.build
 
 import aQute.bnd.gradle.Bundle
-import org.gradle.api.tasks.Input
-import org.gradle.api.tasks.Optional
-import org.osgi.framework.Constants
 
 class OsgiBundle extends Bundle{
-    public String name;
-    public String symbolicName;
-    public String vendor
-    public String developer
-    public String license
-    public String version
-    public String contactAddress
-    public String activator
-
-    public OsgiBundle(){
+    public OsgiBundle()
+    {
         super()
-    }
-
-
-    @Input
-    @Optional
-    String getSymbolicName() {
-        return symbolicName
-    }
-
-    void setSymbolicName(String symbolicName) {
-        this.symbolicName = symbolicName
-        super.convention.plugins.bundle.bnd(Constants.BUNDLE_SYMBOLICNAME+'='+this.symbolicName)
-    }
-
-    public void setName(String name){
-        this.name=name
-        super.convention.plugins.bundle.bnd(Constants.BUNDLE_NAME+"="+this.name)
-    }
-
-    @Input
-    @Optional
-    public String getName(String name){
-        return this.name
-    }
-
-    @Input
-    @Optional
-    String getVendor() {
-        return vendor
-    }
-
-    void setVendor(String vendor) {
-        this.vendor = vendor
-        super.convention.plugins.bundle.bnd(Constants.BUNDLE_VENDOR+'='+this.vendor)
-    }
-
-    @Input
-    @Optional
-    String getDeveloper() {
-        return developer
-    }
-
-    //i was getting compilation error if constants of Bundle Developer was used
-    // i guess this is something to do with version of bundle(org.osgi.framework)
-    // i will try to fix this issue
-    void setDeveloper(String developer) {
-        this.developer = developer
-        super.convention.plugins.bundle.bnd("Bundle-Developers="+this.developer)
-    }
-
-    @Input
-    @Optional
-    String getLicense() {
-        return license
-    }
-
-    void setLicense(String license) {
-        this.license = license
-        super.convention.plugins.bundle.bnd(Constants.BUNDLE_LICENSE+"="+ this.license)
-    }
-
-    @Input
-    @Optional
-    String getVersion() {
-        return version
-    }
-
-    void setVersion(String version) {
-        this.version = version
-        super.convention.plugins.bundle.bnd(Constants.BUNDLE_VERSION+"="+this.version)
-    }
-
-    @Input
-    @Optional
-    String getContactAddress() {
-        return contactAddress
-    }
-
-    void setContactAddress(String contactAddress) {
-        this.contactAddress = contactAddress
-        super.convention.plugins.bundle.bnd(Constants.BUNDLE_CONTACTADDRESS+"="+this.contactAddress)
-    }
-
-    @Input
-    @Optional
-    String getActivator() {
-        return activator
-    }
-
-    void setActivator(String activator) {
-        this.activator = activator
-        super.convention.plugins.bundle.bnd(Constants.BUNDLE_ACTIVATOR+"="+this.activator)
+        this.convention.plugins.insilico=new JarConvention(this)
     }
 }
