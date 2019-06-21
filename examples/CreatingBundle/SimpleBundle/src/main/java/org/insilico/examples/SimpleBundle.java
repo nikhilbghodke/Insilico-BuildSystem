e3c4b5b4ce982b5289263dccd0ad16d411c9699e
Nikhil Ghodke <nikhil.ghodke17@vit.edu>
2019-06-21 12:37:25 -0700

    Created a example project which creates a simple Osgi Bundle

---
+++ examples/CreatingBundle/SimpleBundle/src/main/java/org/insilico/examples/SimpleBundle.java
@@ -0,0 +1,10 @@
+package org.insilico.examples;
+
+public class SimpleBundle {
+    public static void main(String[] args){
+
+        System.out.println("This is a Simple Osgi Bundle");
+
+        return;
+    }
+}
