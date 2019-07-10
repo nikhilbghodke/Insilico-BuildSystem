c361d977a423df1f2606a8b052a7e767ff929ff0
Nikhil Ghodke <nikhil.ghodke17@vit.edu>
2019-07-11 01:40:22 +0530

    Activator-Bundle Example added

--- examples/CreatingBundle/ActivatorBundle/src/main/java/org/insilico/examples/SimpleBundle.java
+++
@@ -1,8 +0,0 @@
-package org.insilico.examples;
-
-public class SimpleBundle {
-    public static void main(String[] args){
-        System.out.println("This is a simple Osgi Bundle");
-        return;
-    }
-}
